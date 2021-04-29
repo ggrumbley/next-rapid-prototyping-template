09d432d9d04cccc1f4fc05a1c1c295ccd9d4f1dd
gary <Gary_Grumbley@intuit.com>
2021-04-29 11:55:18 -0700

    chore: scaffold out todo component

---
+++ components/Todo/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const TodoList: React.FC = () => <div>Todo List goes right here!</div>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div className="w-full h-full bg-gray-300">Build something !!!</div>;
-}
+import { TodoList } from '../components/Todo';
+
+const Home = () => (
+  <div className="grid place-content-center w-full h-full bg-gray-200">
+    <TodoList />
+  </div>
+);
+
+export default Home;
