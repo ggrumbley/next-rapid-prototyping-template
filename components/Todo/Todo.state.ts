804ad664fd2dabdb4272b7349d848b6d8acf44c2
gary <Gary_Grumbley@intuit.com>
2021-04-30 13:25:20 -0700

    feat(todolist): add todo types

--- components/Todo/Todo.state.ts
+++ components/Todo/Todo.state.ts
@@ -3,6 +3,12 @@ import { atom, selector } from 'recoil';
 export const TODO_LIST_STATE = 'TODO_LIST_STATE';
 export const TODO_LIST_FILTER_STATE = 'TODO_LIST_FILTER_STATE';
 
+enum filterStates {
+  SHOW_ALL = 'SHOW_ALL',
+  SHOW_COMPLETED = 'SHOW_COMPLETED',
+  SHOW_UNCOMPLETED = 'SHOW_UNCOMPLETED',
+}
+
 export interface TodoItem {
   id: number;
   text: string;
@@ -12,4 +18,9 @@ export interface TodoItem {
 export const todoListState = atom({
   key: TODO_LIST_STATE,
   default: [],
+});
+
+export const filteredTodoListState = atom({
+  key: TODO_LIST_FILTER_STATE,
+  default: filterStates.SHOW_ALL,
 });
--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,2 @@
 export { supabase } from './initSupabase';
+export { $fetch, initBling } from './bling';
